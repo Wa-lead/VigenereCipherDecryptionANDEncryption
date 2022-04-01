b985452617d76caa2e71c66efc91616dca2d7a3e
Wa-lead <81301826+Wa-lead@users.noreply.github.com>
2022-04-02 01:34:15 +0300

    Update Encrypt.java

--- src/com/company/Encrypt.java
+++ src/com/company/Encrypt.java
@@ -3,7 +3,7 @@ package com.company;
 import java.io.*;
 import java.util.*;
 
-public class Main {
+public class Encrypt {
     static char[] dictionary = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789,.?! ".toCharArray();
     static ArrayList<Character> chars = new ArrayList<>();
 
